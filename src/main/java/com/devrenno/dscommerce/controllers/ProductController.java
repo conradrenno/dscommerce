def09d19760116f2a97b6647f2ef98b6a4b2cebd
conradrenno <conradrenno@hotmail.com>
2025-01-27 23:07:10 -0300

    GET products paged

--- src/main/java/com/devrenno/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devrenno/dscommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devrenno.dscommerce.controllers;
 import com.devrenno.dscommerce.dto.ProductDTO;
 import com.devrenno.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,11 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return productService.findAll(pageable);
+    }
 
     @GetMapping(value = "{id}")
     public ProductDTO findById(@PathVariable Long id){
